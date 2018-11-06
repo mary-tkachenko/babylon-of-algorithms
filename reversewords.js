723eeaa32c22968f07138575b3eebf8ad4a8e493
Mary <l.skyling@gmail.com>
2018-11-06 17:17:37 -0500

    reverse words

---
+++ reversewords.js
@@ -0,0 +1,21 @@
+function reverseWords(str) {
+  return str.split(' ').map(function(word){
+    return word.split('').reverse().join('');
+  }).join(' ');
+}
+
+___________________________
+
+function reverseWords(str) {
+  var splited = str.split(" ");
+  // console.log(splited);
+  var result = '';
+
+for (word of splited) {
+  let piece = word.split('').reverse();
+  console.log(piece.join(''));
+  let yay = piece.join('');
+  console.log(result =  result + ' '+ yay);
+}
+return result.trim();
+}
